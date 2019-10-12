8e06c7321d24ff177f5c96f79ea7923b688f0277
Bartek <kowalczuk.bartek@gmail.com>
2019-10-12 13:33:56 +0200

    initial commit

---
+++ src/CalculatorController.java
@@ -0,0 +1,38 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class CalculatorController {
+
+    private CalculatorView theView;
+    private CalculatorModel theModel;
+
+    public CalculatorController(CalculatorView theView, CalculatorModel theModel) {
+
+        this.theView = theView;
+        this.theModel = theModel;
+
+        this.theView.addCalculateListner(new CalculateListner());
+
+    }
+
+    class CalculateListner implements ActionListener{
+
+        public void actionPerformed(ActionEvent e) {
+            int firstNumber, secondNumber = 0;
+
+            try {
+                firstNumber = theView.getFirstNumber();
+                secondNumber = theView.getSecondNumber();
+
+                theModel.addTwoNumbers(firstNumber, secondNumber);
+
+                theView.setCalcSolution(theModel.getCalculationValue());
+            }
+            catch(NumberFormatException ex){
+                System.out.println(ex);
+                theView.displayErrorMessage("Enter 2 integers");
+            }
+        }
+    }
+
+}
